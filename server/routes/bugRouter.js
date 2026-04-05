b4241a224213aa315641b33b1d13332fbc642bd1
Khushi Jain <125190832+Khushi-J15@users.noreply.github.com>
2026-04-05 10:22:37 +0530

    Delete server directory

--- server/routes/bugRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bugController = require('../controllers/bugController.js');
-
-router.post('/',bugController.reportBug );
-
-module.exports = router;
--- server/routes/testClassifierRouter.js
+++
@@ -1,9 +0,0 @@
-// filepath: /Users/rushilpatel/Desktop/Github/CivicPulse/server/routes/testClassifierRouter.js
-const express = require('express');
-const router = express.Router();
-const testClassifierController = require('../controllers/testClassifierController');
-
-// POST endpoint at the root of this router (will be /api/test-classify/)
-router.post('/', testClassifierController.testClassify);
-
-module.exports = router;
